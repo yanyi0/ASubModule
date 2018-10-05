7d660818771337b76631382da57c2c9b6e6cc5e8
yanyi0 <785144130@qq.com>
2018-10-05 17:04:02 +0800

    初始化子模块
    Signed-off-by: yanyi0 <785144130@qq.com>

---
+++ AModule.h
@@ -0,0 +1,13 @@
+//
+//  AModule.h
+//  GitSubmodules
+//
+//  Created by Cloud on 2018/10/5.
+//  Copyright © 2018年 Cloud. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AModule : NSObject
+
+@end
---
+++ AModule.m
@@ -0,0 +1,13 @@
+//
+//  AModule.m
+//  GitSubmodules
+//
+//  Created by Cloud on 2018/10/5.
+//  Copyright © 2018年 Cloud. All rights reserved.
+//
+
+#import "AModule.h"
+
+@implementation AModule
+
+@end
